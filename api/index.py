32fcadd110cc92e45f0d25690b94d0166c7e03e4
Akash Dubey <69588353+AkeBoss-tech@users.noreply.github.com>
2025-01-07 13:11:11 -0500

    update to use genius api

--- api/index.py
+++ api/index.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify, render_template
 import requests
 from bs4 import BeautifulSoup
+import os
+
+from lyricsgenius import Genius
 
 app = Flask(__name__)
-
-CORS_PROXY = ""
+genius = Genius(os.environ.get('GENIUS_ACCESS_TOKEN'))
+genius.remove_section_headers = True
+genius.skip_non_songs = True
 
 @app.route('/', methods=['GET'])
 def home():
@@ -16,66 +20,35 @@ def search_songs():
     if not query:
         return jsonify({'error': 'No query provided'}), 400
     
-    search_url = f"https://www.google.com/search?q=site:genius.com+{query}"
-    response = requests.get(CORS_PROXY + search_url)
-    
-    if response.status_code != 200:
-        return jsonify({'error': 'Failed to fetch search results'}), 500
-    
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = [a['href'] for a in soup.find_all('a', href=True)]
-    genius_links = [link for link in links if "https://genius.com" in link]
-
-    characters = "%&"
-
-    # ignore all characters after a % or & in the URL
-    for i in range(len(genius_links)):
-        for char in characters:
-            if char in genius_links[i]:
-                genius_links[i] = genius_links[i].split(char)[0]
-    
-    if not genius_links:
-        return jsonify({'results': []})
+    response = genius.search_songs(query, per_page=5)
+    print(response)
     
     results = []
-    for link in genius_links[:3]:  # Limit to 3 results
-        song_title = decode_title(link)
-        results.append({'title': song_title, 'url': link})
+    for thing in response["response"]["hits"]: 
+        if thing.type != "song": continue
+
+        title = thing["result"]["title"]
+        link = thing["result"]["url"]
+        lyric_state = thing["result"]["lyrics_state"]
+        id_value = thing["result"]["id"]
+        image_url = thing["result"]["song_art_image_url"]
+
+        results.append({'title': title, 'link': link, 'lyric_state': lyric_state, 'id': id_value, 'image_url': image_url})
     
     return jsonify({'results': results})
 
 @app.route('/lyrics', methods=['GET'])
 def fetch_lyrics():
+    id_value = request.args.get('id', '').strip()
+    
+    song = genius.lyrics(song_id=id_value)
+    print(song)
 
-    url = request.args.get('url', '').strip()
-    print("URL", url)
-    if not url:
-        return jsonify({'error': 'No URL provided'}), 400
+    if song == None:
+        return jsonify({'error': 'Song not found'})
     
-    response = requests.get(CORS_PROXY + url)
-    """ if response.status_code != 200:
-        print("RESPONSE", response)
-        print("RESPONSE TEXT", response.text)
-        return jsonify({'error': 'Failed to fetch lyrics'}), 500 """
+    return jsonify({'lyrics': song})
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    lyrics_containers = soup.find_all('div', {'data-lyrics-container': 'true'})
-    
-    if not lyrics_containers:
-        return jsonify({'lyrics': 'Lyrics not found.'})
-    
-    lyrics = []
-    for container in lyrics_containers:
-        print("CONTAINER", container)
-        for br in container.find_all("br"):
-            br.replace_with("\n")
-        lyrics.append(container.get_text().strip() + '\n')
-    
-    return jsonify({'lyrics': '\n'.join(lyrics)})
-
-def decode_title(link):
-    song_title = link.split('/')[-1].replace('-', ' ').replace('.html', '')
-    return ' '.join(word.capitalize() for word in song_title.split())
 
 if __name__ == '__main__':
     app.run(debug=True)
